fef86dd5f033c00e190651c8a8e29139696cc3ad
Justin Daining <justin@justindaining.com>
2020-05-06 21:36:59 -0600

    show top card in discard piles

--- src/discard-piles.js
+++ src/discard-piles.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./card";
 import styled from "styled-components";
 
 const DiscardPile = styled.div`
@@ -12,7 +13,24 @@ const DiscardPile = styled.div`
 class DiscardPiles extends React.Component {
   render() {
     return this.props.piles.map((pile, i) => {
-      return <DiscardPile key={i}>{pile.color}</DiscardPile>;
+      const renderDiscardedCards = () => {
+        if (pile.cards.length > 0) {
+          const topCard = pile.cards[0];
+          return (
+            <Card
+              key={i}
+              color={topCard.color}
+              value={topCard.type !== "bet" ? topCard.value : "B"}
+            />
+          );
+        }
+      };
+      return (
+        <DiscardPile key={i}>
+          {pile.color}
+          {renderDiscardedCards()}
+        </DiscardPile>
+      );
     });
   }
 }
--- src/game.js
+++ src/game.js
@@ -47,31 +47,31 @@ export function getInitialState(ctx) {
   return G;
 }
 
-function PlayCard(G, ctx, id) {
-  G.hand[ctx.currentPlayer]--;
-  G.discard++;
-}
+// function PlayCard(G, ctx, id) {
+//   G.hand[ctx.currentPlayer]--;
+//   G.discard++;
+// }
 
-function DrawCard(G, ctx, id) {
-  G.deck--;
-  G.hand[ctx.currentPlayer]++;
-  ctx.events.endTurn();
-}
+// function DrawCard(G, ctx, id) {
+//   G.deck--;
+//   G.hand[ctx.currentPlayer]++;
+//   ctx.events.endTurn();
+// }
 
 const LostSummits = {
   setup: getInitialState,
   playerView: PlayerView.STRIP_SECRETS,
-  turn: {
-    stages: {
-      play: {
-        moves: { PlayCard },
-        next: "discard",
-      },
-      discard: {
-        moves: { DrawCard },
-      },
-    },
-  },
+  // turn: {
+  //   stages: {
+  //     play: {
+  //       moves: { PlayCard },
+  //       next: "discard",
+  //     },
+  //     discard: {
+  //       moves: { DrawCard },
+  //     },
+  //   },
+  // },
 };
 
 export default LostSummits;
