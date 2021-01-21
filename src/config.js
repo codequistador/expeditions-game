5783ea0601ef5b63f8c0d4e3125bb1c5805f6b68
Justin Daining <jdaining@instructure.com>
2021-01-20 22:50:02 -0700

    Better env config

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ export const GAME_NAME = 'LostSummits'
 export const GAME_SERVER_PORT = 8000
 export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`
 export const WEB_SERVER_URL = 'http://localhost:3000'
-export const APP_PRODUCTION = true // TODO: Set this to true when deploying
+export const APP_PRODUCTION =
+  process.env.NODE_ENV === 'production' ? true : false
